67a30e3375319f7f1088a00f6639d4277da4e821
Debayan Mondal <debayanwb17@gmail.com>
2026-04-18 16:40:43 +0530

    Testing

--- src/SideButtons.jsx
+++ src/SideButtons.jsx
@@ -1,6 +1,7 @@
 import './NavBar.css'
 
 //This is a side button
+//More changes
 export default function SideButtons({info,onClick}) {
     return(
         <button onClick={onClick} className="text-[1.2rem] sm:p-0 p-2 cursor-pointer side-btn bg-gray-100 z-0 border-2 border-black min-h-10 w-full rounded-[0.7rem]">{info}</button>
